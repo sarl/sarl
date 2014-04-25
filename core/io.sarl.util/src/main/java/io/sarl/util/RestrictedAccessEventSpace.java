5de996f2143637108974b6afa64951484feb3ab9
sgalland <stephane.galland@utbm.fr>
2014-04-25 22:59:41 +0200

    Add the restricted-access event space.

---
+++ core/io.sarl.util/src/main/java/io/sarl/util/RestrictedAccessEventSpace.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2014 Sebastian RODRIGUEZ, Nicolas GAUD, Stéphane GALLAND.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.sarl.util;
+
+import io.sarl.lang.core.Address;
+import io.sarl.lang.core.EventListener;
+import io.sarl.lang.core.EventSpace;
+
+import java.security.Principal;
+
+
+
+/**
+ * Event driven interaction space where agent are register and unregister themselves
+ * according to a access restriction.
+ * Agents should only register once in this type of space.
+ * 
+ * @author $Author: sgalland$
+ * @version $FullVersion$
+ * @mavengroupid $GroupId$
+ * @mavenartifactid $ArtifactId$
+ */
+public interface RestrictedAccessEventSpace extends EventSpace {
+
+	/**
+	 * Registers the entity inside this space.
+	 * After registering a new agent, the Space should emit a MemberJoined
+	 * event where the source is the address of the newly registered agent.
+	 * 
+	 * If the agent is already registered the address is return, but the listener is not replaced.
+	 * 
+	 * @param entity
+	 * @param requester - object that is requesting the access.
+	 * @return the entity's address in this space
+	 * @fires ParticipantRegistered
+	 */
+	public Address register(EventListener entity, Principal requester);
+
+	/**
+	 * Registers the entity inside this space.
+	 * After registering a new agent, the Space should emit a MemberJoined
+	 * event where the source is the address of the newly registered agent.
+	 * 
+	 * If the agent is already registered the address is return, but the listener is not replaced.
+	 * 
+	 * @param entity - object that is requesting the access.
+	 * @return the entity's address in this space
+	 * @fires ParticipantRegistered
+	 */
+	public <P extends EventListener & Principal> Address register(P entity);
+
+	/**
+	 * Unregisters the entity inside this space.
+	 * Before unregistering an agent, the Space should emit a MemberLeft
+	 * event where the source is the address of the unregistered agent.
+	 * 
+	 * @param entity
+	 * @return the former entity's address
+	 * @fires ParticipantUnregistered
+	 */
+	public Address unregister(EventListener entity);
+	
+}
---
+++ core/io.sarl.util/src/main/java/io/sarl/util/RestrictedAccessEventSpaceSpecification.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2014 Sebastian RODRIGUEZ, Nicolas GAUD, Stéphane GALLAND.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.sarl.util;
+
+import java.io.Serializable;
+import java.security.acl.Permission;
+
+import io.sarl.lang.core.SpaceSpecification;
+
+/**
+ * Specification of an {@link RestrictedAccessEventSpace} where agents are register and unregister themselves
+ * according to a access restriction.
+ * 
+ * @author $Author: sgalland$
+ * @version $FullVersion$
+ * @mavengroupid $GroupId$
+ * @mavenartifactid $ArtifactId$
+ */
+public interface RestrictedAccessEventSpaceSpecification extends SpaceSpecification<RestrictedAccessEventSpace> {
+
+	/** Defines the permission to access to a {@link RestrictedAccessEventSpace space}.
+	 * 
+	 * @author $Author: sgalland$
+	 * @version $FullVersion$
+	 * @mavengroupid $GroupId$
+	 * @mavenartifactid $ArtifactId$
+	 */
+	public static class RegistrationPermission implements Permission, Serializable {
+		
+		private static final long serialVersionUID = 7533477485005863476L;
+
+		/**
+		 */
+		public RegistrationPermission() {
+			//
+		}
+
+		/** {@inheritDoc}
+		 */
+		@Override
+		public boolean equals(Object obj) {
+			if (obj==this) return true;
+			if (obj==null) return false;
+			return toString().equals(obj.toString());
+		}
+		
+		/** {@inheritDoc}
+		 */
+		@Override
+		public int hashCode() {
+			return super.hashCode();
+		}
+		
+		/** {@inheritDoc}
+		 */
+		@Override
+		public String toString() {
+			return "io.sarl.util.RestrictedAccessEventSpace.register"; //$NON-NLS-1$
+		}
+		
+	}
+
+}
