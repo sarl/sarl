c11da8b00a938dde88bcf8da1e8a41e6f3596a89
sgalland <stephane.galland@utbm.fr>
2014-07-07 15:30:43 +0200

    Add the "package" keyword in the new SARL files.

--- plugins/io.sarl.eclipse/src/io/sarl/eclipse/wizards/WizardNewSARLFileCreationPage.java
+++ plugins/io.sarl.eclipse/src/io/sarl/eclipse/wizards/WizardNewSARLFileCreationPage.java
@@ -5,8 +5,17 @@ package io.sarl.eclipse.wizards;
 
 import io.sarl.eclipse.images.EclipseSARLImages;
 
+import java.io.InputStream;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.jdt.core.IClasspathEntry;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.dialogs.WizardNewFileCreationPage;
+import org.eclipse.xtext.util.StringInputStream;
 
 /**
  * First page of the SARL new file wizard.
@@ -36,13 +45,44 @@ public class WizardNewSARLFileCreationPage extends WizardNewFileCreationPage {
         setImageDescriptor(EclipseSARLImages.getImageDescriptor(
         		EclipseSARLImages.NEW_FILE_WIZARD_DIALOG_IMAGE));
     }
- 
-   /* @Override
+    
+    private static IPath determinePackageName(IPath path) {
+    	if (path != null) {
+	    	IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(path.segment(0));
+			try {
+				if (project != null && project.hasNature(JavaCore.NATURE_ID)) {
+					IJavaProject javaProject = JavaCore.create(project);
+					for(IClasspathEntry entry : javaProject.getRawClasspath()) {
+						if (entry.getPath().isPrefixOf(path)) {
+							return path.removeFirstSegments(entry.getPath().segmentCount());
+						}
+					}
+				}
+			} catch(Exception e) {
+				// Ignore the exceptions since they are not useful (hopefully)
+			}
+    	}
+		return null;
+    }
+    
+    @Override
     protected InputStream getInitialContents() {
-        String sarlTemplate = 
-        //Addd the package by default
-        return new ByteArrayInputStream(sarlTemplate.getBytes());
-    }*/
-	
-	
+    	StringBuilder content = new StringBuilder();
+    	
+    	IPath folderInWorkspace = getContainerFullPath();
+    	
+    	IPath packagePath = determinePackageName(folderInWorkspace);
+    	if (packagePath != null && packagePath.segmentCount() > 0) {
+    		content.append("package "); //$NON-NLS-1$
+    		content.append(packagePath.segment(0));
+    		for (int i=1; i<packagePath.segmentCount(); ++i) {
+        		content.append('.');
+        		content.append(packagePath.segment(i));
+    		}
+    		content.append("\n"); //$NON-NLS-1$
+    	}
+    	
+    	return new StringInputStream(content.toString());
+    }	
+    	
 }
