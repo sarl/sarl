97e4d43476fb1e3ac9255d4593cbe234080f66f5
Stéphane Galland <galland@arakhne.org>
2014-10-05 14:56:26 +0200

    [lang][ui] Add accessors for setting the SARL configuration on a project.
    Signed-off-by: Stéphane Galland <galland@arakhne.org>

--- plugins/io.sarl.lang.ui/src/io/sarl/lang/ui/preferences/SARLProjectPreferences.java
+++ plugins/io.sarl.lang.ui/src/io/sarl/lang/ui/preferences/SARLProjectPreferences.java
@@ -25,10 +25,13 @@ import io.sarl.lang.ui.internal.SARLActivator;
 import java.util.Set;
 
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.xtext.builder.EclipseOutputConfigurationProvider;
+import org.eclipse.xtext.builder.preferences.BuilderPreferenceAccess;
 import org.eclipse.xtext.generator.OutputConfiguration;
 import org.eclipse.xtext.ui.editor.preferences.IPreferenceStoreAccess;
+import org.eclipse.xtext.ui.preferences.OptionsConfigurationBlock;
 
 import com.google.inject.Injector;
 
@@ -50,7 +53,7 @@ public final class SARLProjectPreferences {
 	 * @param project - the project.
 	 * @return the preference store or <code>null</code>.
 	 */
-	public static IPreferenceStore getPreferencesFor(IProject project) {
+	public static IPreferenceStore getSARLPreferencesFor(IProject project) {
 		if (project != null) {
 			Injector injector = SARLActivator.getInstance().getInjector(SARLActivator.IO_SARL_LANG_SARL);
 			IPreferenceStoreAccess preferenceStoreAccess = injector.getInstance(IPreferenceStoreAccess.class);
@@ -69,6 +72,77 @@ public final class SARLProjectPreferences {
 		EclipseOutputConfigurationProvider configurationProvider =
 				injector.getInstance(EclipseOutputConfigurationProvider.class);
 		return configurationProvider.getOutputConfigurations(project);
+	}
+
+	/** Configure the given project for using the system-wide
+	 * configuration related to SARL.
+	 *
+	 * @param project - the project.
+	 */
+	public static void setSystemSARLConfigurationFor(IProject project) {
+		IPreferenceStore preferenceStore = SARLProjectPreferences.getSARLPreferencesFor(project);
+		preferenceStore.setValue(OptionsConfigurationBlock.IS_PROJECT_SPECIFIC, false);
+	}
+
+	/** Configure the given project for using a specific configuration
+	 * related to SARL.
+	 *
+	 * @param project - the project.
+	 * @param outputPath - the path where SARL compiler is generating the Java code.
+	 */
+	public static void setSpecificSARLConfigurationFor(
+			IProject project,
+			IPath outputPath) {
+		IPreferenceStore preferenceStore = SARLProjectPreferences.getSARLPreferencesFor(project);
+		// Force to use a specific configuration for the SARL
+		preferenceStore.setValue(OptionsConfigurationBlock.IS_PROJECT_SPECIFIC, true);
+
+		// Loop on the Xtext configurations embeded in the SARL compiler.
+		String key;
+		for (OutputConfiguration projectConfiguration
+				: SARLProjectPreferences.getXtextConfigurationsFor(project)) {
+			//
+			// OUTPUT PATH
+			key = BuilderPreferenceAccess.getKey(
+					projectConfiguration,
+					EclipseOutputConfigurationProvider.OUTPUT_DIRECTORY);
+			preferenceStore.setValue(key, outputPath.toOSString());
+
+			//
+			// CREATE THE OUTPUT DIRECTORY
+			key = BuilderPreferenceAccess.getKey(
+					projectConfiguration,
+					EclipseOutputConfigurationProvider.OUTPUT_CREATE_DIRECTORY);
+			preferenceStore.setValue(key, true);
+
+			//
+			// OVERWRITE THE EXISTING FILES
+			key = BuilderPreferenceAccess.getKey(
+					projectConfiguration,
+					EclipseOutputConfigurationProvider.OUTPUT_OVERRIDE);
+			preferenceStore.setValue(key, true);
+
+			//
+			// SET GENERATED FILES AS DERIVED
+			key = BuilderPreferenceAccess.getKey(
+					projectConfiguration,
+					EclipseOutputConfigurationProvider.OUTPUT_DERIVED);
+			preferenceStore.setValue(key, true);
+
+			//
+			// CLEAN THE GENERATED FILES
+			key = BuilderPreferenceAccess.getKey(
+					projectConfiguration,
+					EclipseOutputConfigurationProvider.OUTPUT_CLEANUP_DERIVED);
+			preferenceStore.setValue(key, true);
+
+			//
+			// CLEAN THE OUTPUT DIRECTORY
+			key = BuilderPreferenceAccess.getKey(
+					projectConfiguration,
+					EclipseOutputConfigurationProvider.OUTPUT_CLEAN_DIRECTORY);
+			preferenceStore.setValue(key, true);
+		}
 	}
 
 }
