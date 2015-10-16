b33baa4ff5d2da0dbbd70fedd6cafb65f0fcf011
Stéphane Galland <galland@arakhne.org>
2015-10-16 14:05:15 +0200

    [lang] Add 'it' in the event's guards.
    see #323
    
    Signed-off-by: Stéphane Galland <galland@arakhne.org>

--- tests/io.sarl.lang.tests/src/io/sarl/lang/tests/parsing/aop/AgentParsingTest.java
+++ tests/io.sarl.lang.tests/src/io/sarl/lang/tests/parsing/aop/AgentParsingTest.java
@@ -453,6 +453,32 @@ public class AgentParsingTest {
 	public static class BehaviorUnitTest extends AbstractSarlTest {
 
 		@Test
+		public void itInsideEventHandlerGuards_explicitIt() throws Exception {
+			SarlScript mas = file(multilineString(
+					"event E {",
+					"  var field : int",
+					"}",
+					"agent A1 {",
+					"  on E [ it.field == 4 ] {",
+					"  }",
+					"}"
+					), true);
+		}
+
+		@Test
+		public void itInsideEventHandlerGuards_implicitIt() throws Exception {
+			SarlScript mas = file(multilineString(
+					"event E {",
+					"  var field : int",
+					"}",
+					"agent A1 {",
+					"  on E [ field == 4 ] {",
+					"  }",
+					"}"
+					), true);
+		}
+
+		@Test
 		public void synchronizedGuard() throws Exception {
 			SarlScript mas = file(multilineString(
 					"event E {}",
