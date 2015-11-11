5f2a7809d152639b50cacff70545035602183502
Stéphane Galland <galland@arakhne.org>
2015-11-11 15:41:30 +0100

    [maven] Add unit test with a failing pom file.
    see #317
    
    Signed-off-by: Stéphane Galland <galland@arakhne.org>

---
+++ tests/sarl-maven-plugin.tests/src/test/java/io/sarl/maven/compiler/Bug317.java
@@ -0,0 +1,54 @@
+/*
+ * $Id$
+ *
+ * SARL is an general-purpose agent programming language.
+ * More details on http://www.sarl.io
+ *
+ * Copyright (C) 2014-2015 the original authors or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.sarl.maven.compiler;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+import org.apache.maven.it.VerificationException;
+
+import org.apache.maven.it.Verifier;
+import org.apache.maven.plugin.MojoFailureException;
+import org.junit.Test;
+
+/**
+ * @author $Author: sgalland$
+ * @version $FullVersion$
+ * @mavengroupid $GroupId$
+ * @mavenartifactid $ArtifactId$
+ */
+@SuppressWarnings("all")
+public class Bug317 extends AbstractMojoTest {
+
+	@Test
+	public void compile() throws Exception {
+		Verifier verifier = executeMojo("prj3", "compile");
+		Path path = FileSystems.getDefault().getPath(
+				"src", "main", "generated-sources", "sarl",
+				"io", "sarl", "maven", "compiler", "tests", "MyAgent.java");
+		assertNotNull(path);
+		verifier.assertFilePresent(path.toString());
+	}
+
+}
