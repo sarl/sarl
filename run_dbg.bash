f4715aa2e3fb88cb9f586a3aacdbd69ba4a7c677
Stéphane Galland <galland@arakhne.org>
2015-11-21 19:05:03 +0100

    Remove unnecessary files on Git.
    Signed-off-by: Stéphane Galland <galland@arakhne.org>

--- run_dbg.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-SCRIPT=`pwd`/releng/io.sarl.lang.updatesite/target/products/io.sarl.lang.product/linux/gtk/x86_64/eclipse-sarl-ubuntu.sh
-
-chmod +x "$SCRIPT"
-exec "$SCRIPT" 2>&1 | tee `pwd`/dbg.log
