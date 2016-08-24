3b51755252a36190a722d827e432c007bfca8c3f
Stéphane Galland <galland@arakhne.org>
2016-08-24 16:20:25 +0200

    [eclipse] Create the SARL perspective.
    close #142
    
    Signed-off-by: Stéphane Galland <galland@arakhne.org>

--- eclipse-sarl/plugins/io.sarl.eclipse/src/io/sarl/eclipse/SARLEclipseConfig.java
+++ eclipse-sarl/plugins/io.sarl.eclipse/src/io/sarl/eclipse/SARLEclipseConfig.java
@@ -50,6 +50,11 @@ public final class SARLEclipseConfig {
 	public static final String NATURE_ID = "io.sarl.eclipse.SARLProjectNature"; //$NON-NLS-1$
 
 	/**
+	 * ID of the SARL perspective.
+	 */
+	public static final String SARL_PERSPECTIVE_ID = "io.sarl.eclipse.perspective.devel"; //$NON-NLS-1$
+
+	/**
 	 * The value is the identifier of the class path container for
 	 * the SARL runtime environment.
 	 */
