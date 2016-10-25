53276e6a9a5cea17113f3802f7952f572b6b1d69
Jérôme BOULMIER <jerome.boulmier@outlook.fr>
2016-10-25 12:19:07 +0200

    Added cell agent action class.

---
+++ src/main/sarl/io/sarl/demos/gameoflife/environment/agent/Action.java
@@ -0,0 +1,46 @@
+package io.sarl.demos.gameoflife.environment.agent;
+
+import java.util.UUID;
+
+import org.arakhne.afc.math.geometry.d2.i.Vector2i;
+
+public class Action {
+	public enum Type {
+		CREATED,
+		DESTROYED
+	}
+	
+	private final UUID emitter;
+	private final Vector2i position;
+	private final Type type;
+	
+	Action(UUID emitter, Vector2i position, Type type) {
+		this.emitter = emitter;
+		this.position = position;
+		this.type = type;
+	}
+	
+	/**
+	 * Replies the emitter.
+	 * @return
+	 */
+	public UUID getEmitter() {
+		return this.emitter;
+	}
+	
+	/**
+	 * Replies the position of the emitter.
+	 * @return
+	 */
+	public Vector2i getPosition() {
+		return this.position;
+	}
+
+	/**
+	 * Replies the type of the event.
+	 * @return
+	 */
+	public Type getType() {
+		return type;
+	}
+}
