dff1651a9c584040c66c65087e8f4eea656940d5
pinam45 <maxime.pin@live.fr>
2016-10-27 11:59:08 +0200

    New GUI

--- src/main/sarl/io/sarl/demos/gameoflife/gui/GUI.java
+++ src/main/sarl/io/sarl/demos/gameoflife/gui/GUI.java
@@ -5,6 +5,11 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 import org.eclipse.xtext.xbase.lib.Pair;
@@ -24,9 +29,22 @@ public class GUI extends Application implements EnvironmentListener {
 
 	private static GUI gui;
 	private final List<GUIListener> listeners = new ArrayList<>();
-	private SquareGridDisplayer squareGridDisplayer;
 	private Stage primaryStage;
-	private boolean launched = false;
+	private final Label widthLabel = new Label("Width:");
+	private final RestrictedNumberTextField widthTextField = new RestrictedNumberTextField(5, 1, 100);
+	private final Label heightLabel = new Label("Height:");
+	private final RestrictedNumberTextField heightTextField = new RestrictedNumberTextField(5, 1, 100);
+	private final Button setupButton = new Button("Setup");
+	private final Button playButton = new Button("Play");
+	private final Button pauseButton = new Button("Pause");
+	private final Button exitButton = new Button("Exit");
+	private final HBox buttonsHbox = new HBox(this.widthLabel, this.widthTextField, this.heightLabel, this.heightTextField, this.setupButton, this.playButton, this.pauseButton, this.exitButton);
+	private SquareGridDisplayer squareGridDisplayer;
+	private final Pane squareGridDisplayerPane = new Pane();
+	private final VBox vBox = new VBox(this.buttonsHbox, this.squareGridDisplayerPane);
+	private boolean inited = false;
+	private int gridWidth;
+	private int gridHeight;
 
 	/**
 	 * Gets gui.
@@ -48,30 +66,46 @@ public class GUI extends Application implements EnvironmentListener {
 
 	/**
 	 * Launch the gui.
-	 *
-	 * @param width  the width
-	 * @param height the height
 	 */
-	public void launchGUI(int width, int height) {
+	public void launchGUI() {
 
-		Platform.runLater(() -> {
-			if(!this.launched) {
-				this.primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-					@Override
-					public void handle(WindowEvent event) {
-						GUI.this.listeners.forEach(GUIListener::stop);
-					}
-				});
-				this.primaryStage.setTitle("Sarl game of life demo");
+		Platform.runLater(this::initGUI);
+	}
 
-				this.squareGridDisplayer = new SquareGridDisplayer(width, height);
-				Scene scene = new Scene(this.squareGridDisplayer);
+	private void initGUI() {
+		if(!this.inited) {
+			this.primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+				@Override
+				public void handle(WindowEvent event) {
+					GUI.this.listeners.forEach(GUIListener::stop);
+				}
+			});
+			this.primaryStage.setTitle("Sarl game of life demo");
 
-				this.primaryStage.setScene(scene);
-				this.primaryStage.show();
-				this.launched = true;
-			}
-		});
+			Scene scene = new Scene(this.vBox);
+
+			this.primaryStage.setScene(scene);
+			this.primaryStage.show();
+			this.inited = true;
+		}
+	}
+
+	/**
+	 * Setup the gui.
+	 *
+	 * @param width  the grid width
+	 * @param height the grid height
+	 */
+	public void setupGUI(int width, int height) {
+		if(this.inited) {
+			this.gridWidth = width;
+			this.gridHeight = height;
+			this.squareGridDisplayer = new SquareGridDisplayer(this.gridWidth, this.gridHeight);
+			Platform.runLater(() -> {
+				this.squareGridDisplayerPane.getChildren().removeAll();
+				this.squareGridDisplayerPane.getChildren().add(this.squareGridDisplayer);
+			});
+		}
 	}
 
 	@Override
@@ -91,12 +125,17 @@ public class GUI extends Application implements EnvironmentListener {
 
 	@Override
 	public void handleGridUpdate(List<List<Pair<UUID, Boolean>>> grid) {
+
+		if(!this.inited) {
+			launchGUI();
+		}
+
 		if(grid.size() == 0 || grid.get(0).size() == 0) {
 			throw new IllegalArgumentException("grid width or grid height is equal to 0");
 		}
 
-		if(!this.launched) {
-			launchGUI(grid.size(), grid.get(0).size());
+		if(grid.size() != this.gridWidth || grid.get(0).size() != this.gridHeight) {
+			setupGUI(grid.size(), grid.get(0).size());
 		}
 
 		boolean[][] booleenGrid = new boolean[grid.size()][grid.get(0).size()];
---
+++ src/main/sarl/io/sarl/demos/gameoflife/gui/RestrictedNumberTextField.java
@@ -0,0 +1,37 @@
+package io.sarl.demos.gameoflife.gui;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.control.TextField;
+
+/**
+ * The type RestrictedNumberTextField.
+ *
+ * @author Maxime PINARD
+ */
+public class RestrictedNumberTextField extends TextField {
+
+	private final int minValue;
+	private final int maxValue;
+
+	public RestrictedNumberTextField(int defaultValue, int minValue, int maxValue) {
+		super(Integer.toString(defaultValue));
+		this.minValue = minValue;
+		this.maxValue = maxValue;
+		textProperty().addListener(new ChangeListener<String>() {
+			@Override
+			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+				if(newValue.matches("[0-9]*") && validateValue(Integer.parseInt(newValue))) {
+					setText(newValue);
+				}
+				else {
+					setText(oldValue);
+				}
+			}
+		});
+	}
+
+	private boolean validateValue(int value) {
+		return (value >= this.minValue && value <= this.maxValue);
+	}
+}
