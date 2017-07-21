9c7b261ef5115ea0f9c4cdb8fbfaa09c40fef8f5
Stéphane Galland <galland@arakhne.org>
2017-07-21 22:37:37 +0200

    [ci] Use Maven 3.5.0 on CI servers.
    Signed-off-by: Stéphane Galland <galland@arakhne.org>

--- build-tools/scripts/travis-download-maven.sh
+++ build-tools/scripts/travis-download-maven.sh
@@ -3,15 +3,15 @@
 mkdir -p $HOME/.binaries
 cd $HOME/.binaries
 
-if [ '!' -d apache-maven-3.3.9 ]
+if [ '!' -d apache-maven-3.5.0 ]
 then
-  if [ '!' -f apache-maven-3.3.9-bin.zip ]
+  if [ '!' -f apache-maven-3.5.0-bin.zip ]
   then 
-    wget https://archive.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.zip || exit 1
+    wget https://archive.apache.org/dist/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.zip || exit 1
   fi
-  echo "Installing maven 3.3.9"
-  unzip -qq apache-maven-3.3.9-bin.zip || exit 1
-  rm -f apache-maven-3.3.9-bin.zip
+  echo "Installing maven 3.5.0"
+  unzip -qq apache-maven-3.5.0-bin.zip || exit 1
+  rm -f apache-maven-3.5.0-bin.zip
 fi
 
 exit 0
