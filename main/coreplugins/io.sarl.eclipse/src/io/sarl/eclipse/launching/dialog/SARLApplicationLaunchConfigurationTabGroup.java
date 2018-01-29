cb5206671cfef9e8421cf2cb83c54650ea1ecdda
Stéphane Galland <galland@arakhne.org>
2018-01-29 15:09:14 +0100

    [eclipse] Add SARL application launch configuration.
    see #745
    
    Signed-off-by: Stéphane Galland <galland@arakhne.org>

---
+++ main/coreplugins/io.sarl.eclipse/src/io/sarl/eclipse/launching/dialog/SARLApplicationLaunchConfigurationTabGroup.java
@@ -0,0 +1,56 @@
+/*
+ * $Id$
+ *
+ * SARL is an general-purpose agent programming language.
+ * More details on http://www.sarl.io
+ *
+ * Copyright (C) 2014-2018 the original authors or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.sarl.eclipse.launching.dialog;
+
+import org.eclipse.debug.ui.CommonTab;
+import org.eclipse.debug.ui.EnvironmentTab;
+import org.eclipse.debug.ui.ILaunchConfigurationDialog;
+import org.eclipse.debug.ui.ILaunchConfigurationTab;
+import org.eclipse.debug.ui.sourcelookup.SourceLookupTab;
+import org.eclipse.jdt.debug.ui.launchConfigurations.JavaArgumentsTab;
+
+/**
+ * Tab group object for configuring the run of a Java application embedding SARL.
+ *
+ * @author $Author: sgalland$
+ * @version $FullVersion$
+ * @mavengroupid $GroupId$
+ * @mavenartifactid $ArtifactId$
+ * @since 0.7
+ */
+public class SARLApplicationLaunchConfigurationTabGroup extends AbstractSARLLaunchConfigurationTabGroup {
+
+	@Override
+	public void createTabs(ILaunchConfigurationDialog dialog, String mode) {
+		final ILaunchConfigurationTab[] tabs = new ILaunchConfigurationTab[] {
+			new SARLApplicationMainLaunchConfigurationTab(),
+			new JavaArgumentsTab(),
+			new SARLRuntimeEnvironmentTab(false),
+			getClasspathTab(dialog),
+			new SourceLookupTab(),
+			new EnvironmentTab(),
+			new CommonTab(),
+		};
+		setTabs(tabs);
+	}
+
+}
