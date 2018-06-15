c82ef2a00d33f79c88d488f63f6b0b2dbae26935
Stéphane Galland <galland@arakhne.org>
2018-06-15 23:00:31 +0200

    [ci] Upgrade to Maven 3.5.3.
    Signed-off-by: Stéphane Galland <galland@arakhne.org>

--- build-tools/scripts/travis-download-maven.sh
+++ build-tools/scripts/travis-download-maven.sh
@@ -3,15 +3,15 @@
 mkdir -p $HOME/.binaries
 cd $HOME/.binaries
 
-if [ '!' -d apache-maven-3.5.2 ]
+if [ '!' -d apache-maven-3.5.3 ]
 then
-  if [ '!' -f apache-maven-3.5.2-bin.zip ]
+  if [ '!' -f apache-maven-3.5.3-bin.zip ]
   then 
-    wget https://archive.apache.org/dist/maven/maven-3/3.5.2/binaries/apache-maven-3.5.2-bin.zip || exit 1
+    wget https://archive.apache.org/dist/maven/maven-3/3.5.3/binaries/apache-maven-3.5.3-bin.zip || exit 1
   fi
-  echo "Installing maven 3.5.2"
-  unzip -qq apache-maven-3.5.2-bin.zip || exit 1
-  rm -f apache-maven-3.5.2-bin.zip
+  echo "Installing maven 3.5.3"
+  unzip -qq apache-maven-3.5.3-bin.zip || exit 1
+  rm -f apache-maven-3.5.3-bin.zip
 fi
 
 exit 0
