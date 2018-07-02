a4c89a42429e280496051ce16d747f8cefbe3754
Stéphane Galland <galland@arakhne.org>
2018-07-02 14:19:15 +0200

    [product] Linux launching scripts may support both GTK2 and GTK3.
    Signed-off-by: Stéphane Galland <galland@arakhne.org>

--- products/updatesite/rootfiles/linux/eclipse-sarl-archlinux.sh
+++ products/updatesite/rootfiles/linux/eclipse-sarl-archlinux.sh
@@ -5,16 +5,6 @@ echo "MYSELF=$MYSELF"
 CDIR=`dirname "$MYSELF"`
 echo "CDIR=$CDIR"
 
-if [ -r "$HOME/.eclipse/eclipserc" ]
-then
-	echo "Loading user configuration"
-	. "$HOME/.eclipse/eclipserc"
-elif [ -r "$HOME/.eclipserc" ]
-then
-	echo "Loading user configuration"
-	. "$HOME/.eclipserc"
-fi
-
 # Use GTK 2
 export SWT_GTK=0
 
@@ -24,6 +14,16 @@ export LIBOVERLAY_SCROLLBAR=0
 # Remove /usr/lib/jni from LD_LIBRARY_PATH to avoid loading from Eclipse
 export LD_LIBRARY_PATH=`echo "$LD_LIBRARY_PATH" | perl -p -e 's!(/usr/lib/jni:)|(:?/usr/lib/jni)!!'`
 echo "Native Library Path: $LD_LIBRARY_PATH"
+
+if [ -r "$HOME/.eclipse/eclipserc" ]
+then
+	echo "Loading user configuration"
+	. "$HOME/.eclipse/eclipserc"
+elif [ -r "$HOME/.eclipserc" ]
+then
+	echo "Loading user configuration"
+	. "$HOME/.eclipserc"
+fi
 
 echo "Launching SARL IDE ${project.version}"
 exec "$CDIR/eclipse-sarl" $VMARGS "$@"
