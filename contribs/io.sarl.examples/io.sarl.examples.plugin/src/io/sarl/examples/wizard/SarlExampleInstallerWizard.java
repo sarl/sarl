28f946c5b778bd50c133b0c50b1a819351c5bc12
Stéphane Galland <galland@arakhne.org>
2018-08-22 14:04:38 +0200

    [examples] Create a Maven project when the example folder contains a pom file.
    Signed-off-by: Stéphane Galland <galland@arakhne.org>

--- contribs/io.sarl.examples/io.sarl.examples.plugin/src/io/sarl/examples/wizard/SarlExampleInstallerWizard.java
+++ contribs/io.sarl.examples/io.sarl.examples.plugin/src/io/sarl/examples/wizard/SarlExampleInstallerWizard.java
@@ -21,15 +21,29 @@
 
 package io.sarl.examples.wizard;
 
-import org.eclipse.core.runtime.CoreException;
+import java.io.File;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.SubMonitor;
+import org.eclipse.debug.internal.ui.SWTFactory;
 import org.eclipse.emf.common.ui.wizard.ExampleInstallerWizard;
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.intro.IIntroManager;
 import org.eclipse.ui.intro.IIntroPart;
+import org.eclipse.ui.wizards.datatransfer.ImportOperation;
 
 import io.sarl.eclipse.natures.SARLProjectConfigurator;
+import io.sarl.m2e.utils.M2EUtilities;
 
 /** Wizard for importing SARL samples.
  *
@@ -43,19 +57,66 @@ import io.sarl.eclipse.natures.SARLProjectConfigurator;
  */
 public class SarlExampleInstallerWizard extends ExampleInstallerWizard {
 
+	private ConfigurationPage configurationPage;
+
 	@Override
-	protected void createProject(ProjectDescriptor projectDescriptor, IProgressMonitor monitor) throws CoreException {
-		final SubMonitor mon = SubMonitor.convert(monitor, 2);
-		super.createProject(projectDescriptor, mon.newChild(1));
-		SARLProjectConfigurator.configureSARLProject(
-				// Project to configure
-				projectDescriptor.getProject(),
-				// Force java configuration
-				true,
-				// Create folders
-				true,
-				// Monitor
-				mon.newChild(1));
+	public void addPages() {
+		// Override the default "addPages" in order to create a configuration page.
+		super.addPages();
+		this.configurationPage = new ConfigurationPage("configurationPage", Messages.SarlExampleInstallerWizard_0); //$NON-NLS-1$
+		this.configurationPage.setDescription(Messages.SarlExampleInstallerWizard_1);
+		addPage(this.configurationPage);
+	}
+
+	@Override
+	public void dispose() {
+		this.configurationPage.dispose();
+		this.configurationPage = null;
+		super.dispose();
+	}
+
+	@Override
+	protected void installProject(ProjectDescriptor projectDescriptor, ImportOperation importOperation,
+			IProgressMonitor progressMonitor) throws Exception {
+		final SubMonitor mon = SubMonitor.convert(progressMonitor, 5);
+
+		// Standard creation
+		super.installProject(projectDescriptor, importOperation, progressMonitor);
+
+		final IProject project = projectDescriptor.getProject();
+
+		final IFile pomFile = project.getFile(Path.fromOSString("pom.xml")); //$NON-NLS-1$
+		if (this.configurationPage.isMavenNatureEnabled() && pomFile.exists()) {
+			// The project should be a Maven project.
+			final IPath descriptionFilename = project.getFile(new Path(IProjectDescription.DESCRIPTION_FILE_NAME)).getLocation();
+			final File projectDescriptionFile = descriptionFilename.toFile();
+			// Project was open by the super class. Close it because Maven fails when a project already exists.
+			project.close(mon.newChild(1));
+			// Delete the Eclipse project definition because Maven fails when a project already exists.
+			project.delete(false, true, mon.newChild(1));
+			if (projectDescriptionFile.exists()) {
+				projectDescriptionFile.delete();
+			}
+			// Import
+			M2EUtilities.importMavenProject(
+					project.getWorkspace().getRoot(),
+					projectDescriptor.getName(),
+					true,
+					mon.newChild(1));
+		} else {
+			// Force the project configuration to SARL.
+			SARLProjectConfigurator.configureSARLProject(
+					// Project to configure
+					project,
+					// Add SARL natures
+					true,
+					// Force java configuration
+					true,
+					// Create folders
+					true,
+					// Monitor
+					mon.newChild(3));
+		}
 		mon.done();
 	}
 
@@ -65,6 +126,9 @@ public class SarlExampleInstallerWizard extends ExampleInstallerWizard {
 			// Close the welcome page.
 			closeWelcomePage();
 			return true;
+		}
+		if (this.configurationPage != null && !this.configurationPage.getControl().isDisposed()) {
+			this.configurationPage.refresh();
 		}
 		return false;
 	}
@@ -79,6 +143,69 @@ public class SarlExampleInstallerWizard extends ExampleInstallerWizard {
 				introManager.closeIntro(intro);
 			}
 		}
+	}
+
+	/** Configuration page.
+	 *
+	 * @author $Author: sgalland$
+	 * @version $FullVersion$
+	 * @mavengroupid $GroupId$
+	 * @mavenartifactid $ArtifactId$
+	 * @since 0.8
+	 */
+	public class ConfigurationPage extends WizardPage {
+
+		/** Indicates if the maven nature must be enabled by default.
+		 */
+		public static final boolean DEFAULT_MAVEN_NATURE = true;
+
+		private Button mavenProjectButton;
+
+		/** Constructor.
+		 *
+		 * @param pageName the name of the page.
+		 * @param title the title.
+		 */
+		public ConfigurationPage(String pageName, String title) {
+			super(pageName, title, null);
+		}
+
+		@Override
+		public void createControl(Composite parent) {
+			final Composite composite = new Composite(parent, SWT.NONE);
+			final GridLayout layout = new GridLayout(2, false);
+			final int margin = -5;
+			final int spacing = 3;
+			layout.marginTop = margin;
+			layout.marginLeft = margin;
+			layout.marginRight = margin;
+			layout.marginBottom = margin;
+			layout.horizontalSpacing = spacing;
+			layout.verticalSpacing = spacing;
+			composite.setLayout(layout);
+
+			this.mavenProjectButton = SWTFactory.createCheckButton(composite,
+					Messages.SarlExampleInstallerWizard_2, null, DEFAULT_MAVEN_NATURE, 2);
+
+			refresh();
+			setControl(composite);
+		}
+
+		/** Refresh the configuration page.
+		 */
+		public void refresh() {
+			setErrorMessage(null);
+			setPageComplete(true);
+		}
+
+		/** Replies if the maven nature must be enabled.
+		 *
+		 * @return {@code true} for enabling the maven nature.
+		 */
+		public boolean isMavenNatureEnabled() {
+			return this.mavenProjectButton.getSelection();
+		}
+
 	}
 
 }
