a250aa4ae967826cef6fc5b081af69b3eeb8a55a
Stéphane Galland <galland@arakhne.org>
2018-10-04 15:14:52 +0200

    [ci] Fixing the Travis configuration.
    In order to pass the compilation on the Travis CI server, the
    configuration is fixed.
    
    see #869
    see #812
    
    Signed-off-by: Stéphane Galland <galland@arakhne.org>

--- build-tools/scripts/travis-download-maven.sh
+++ build-tools/scripts/travis-download-maven.sh
@@ -3,15 +3,15 @@
 mkdir -p $HOME/.binaries
 cd $HOME/.binaries
 
-if [ '!' -d apache-maven-3.5.3 ]
+if [ '!' -d apache-maven-3.5.4 ]
 then
-  if [ '!' -f apache-maven-3.5.3-bin.zip ]
+  if [ '!' -f apache-maven-3.5.4-bin.zip ]
   then 
-    wget https://archive.apache.org/dist/maven/maven-3/3.5.3/binaries/apache-maven-3.5.3-bin.zip || exit 1
+    wget http://www.us.apache.org/dist/maven/maven-3/3.5.4/binaries/apache-maven-3.5.4-bin.zip || exit 1
   fi
-  echo "Installing maven 3.5.3"
-  unzip -qq apache-maven-3.5.3-bin.zip || exit 1
-  rm -f apache-maven-3.5.3-bin.zip
+  echo "Installing maven 3.5.4"
+  unzip -qq apache-maven-3.5.4-bin.zip || exit 1
+  rm -f apache-maven-3.5.4-bin.zip
 fi
 
 exit 0
