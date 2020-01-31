a91b3de04b9caf10e2ad06cb0b2f001421af8094
Stéphane Galland <galland@arakhne.org>
2020-01-31 16:31:38 +0100

    [lang] Remove reference to JDT in "io.sarl.lang" module.
    Signed-off-by: Stéphane Galland <galland@arakhne.org>

--- main/coreplugins/io.sarl.lang/src/io/sarl/lang/util/Utils.java
+++ main/coreplugins/io.sarl.lang/src/io/sarl/lang/util/Utils.java
@@ -44,7 +44,6 @@ import org.eclipse.emf.common.notify.Notifier;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.util.EcoreUtil;
-import org.eclipse.jdt.core.Flags;
 import org.eclipse.xtend.core.jvmmodel.XtendJvmModelInferrer;
 import org.eclipse.xtend.core.xtend.XtendFunction;
 import org.eclipse.xtend.core.xtend.XtendMember;
@@ -1457,7 +1456,7 @@ public final class Utils {
 			boolean firstRound = true;
 
 			for (final Field field : fields) {
-				if (!includeStaticField && Flags.isStatic(field.getModifiers())) {
+				if (!includeStaticField && Modifier.isStatic(field.getModifiers())) {
 					continue;
 				}
 				if (!firstRound) {
