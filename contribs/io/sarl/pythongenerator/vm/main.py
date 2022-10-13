4b31827f3de2fb1538d328dfabe7cf83866ee140
h-loic <h-loic@orange.fr>
2022-10-13 21:01:29 +0200

    created python code for LoggingAgent

--- contribs/io/sarl/pythongenerator/vm/main.py
+++ contribs/io/sarl/pythongenerator/vm/main.py
@@ -1,16 +1,13 @@
-from contribs.io.sarl.pythongenerator.vm.helloAgent import HelloAgent
-from contribs.io.sarl.pythongenerator.vm.helloWorld import HelloWorldAgent
+from contribs.io.sarl.pythongenerator.vm.HelloWorld.helloWorld import HelloWorldAgent
+from contribs.io.sarl.pythongenerator.vm.LoggingAgent.LoggingAgent import LoggingAgent
 
 ### HelloWorld
-helloWorld = HelloWorldAgent
-eventsList = helloWorld.__guard_io_sarl_core_Initialize__(helloWorld,1)
-eventsList[0](helloWorld,1)
-
+# helloWorld = HelloWorldAgent
+# eventsList = helloWorld.__guard_io_sarl_core_Initialize__(helloWorld,1)
+# eventsList[0](helloWorld,1)
 
 ### HelloWorld with the loggingSkill
-"""
-myAgent = HelloAgent
-myAgentEvents = myAgent.__guard_io_sarl_core_Initialize__(myAgent,1)
-myAgentEvents[0](myAgent,1)
-"""
+loggingAgent = LoggingAgent
+myAgentEvents = loggingAgent.__guard_io_sarl_core_Initialize__(loggingAgent,1)
+myAgentEvents[0](loggingAgent,1)
 
