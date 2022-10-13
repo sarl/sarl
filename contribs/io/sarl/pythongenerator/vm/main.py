e69c4d801c2e8662e2e9ae7d8bcc1a1d0e2fc78f
h-loic <h-loic@orange.fr>
2022-10-13 20:29:42 +0200

    created HelloWordAgent

--- contribs/io/sarl/pythongenerator/vm/main.py
+++ contribs/io/sarl/pythongenerator/vm/main.py
@@ -1,7 +1,16 @@
 from contribs.io.sarl.pythongenerator.vm.helloAgent import HelloAgent
+from contribs.io.sarl.pythongenerator.vm.helloWorld import HelloWorldAgent
 
+### HelloWorld
+helloWorld = HelloWorldAgent
+eventsList = helloWorld.__guard_io_sarl_core_Initialize__(helloWorld,1)
+eventsList[0](helloWorld,1)
+
+
+### HelloWorld with the loggingSkill
+"""
 myAgent = HelloAgent
 myAgentEvents = myAgent.__guard_io_sarl_core_Initialize__(myAgent,1)
 myAgentEvents[0](myAgent,1)
-
+"""
 
