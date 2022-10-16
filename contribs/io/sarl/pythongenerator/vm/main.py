5bda936b5b8031c3274192d25102e69efc2e76d7
Antoine RICHARD <antoine.richard68@gmail.com>
2022-10-16 11:32:11 +0200

    using uuid to generate unique ids

--- contribs/io/sarl/pythongenerator/vm/main.py
+++ contribs/io/sarl/pythongenerator/vm/main.py
@@ -2,17 +2,19 @@ from contribs.io.sarl.pythongenerator.vm.GuardedAgent.guardedAgent import Guarde
 #from contribs.io.sarl.pythongenerator.vm.HelloWorld.helloWorld import HelloWorldAgent
 from contribs.io.sarl.pythongenerator.vm.LoggingAgent.LoggingAgent import LoggingAgent
 
+import uuid
+
 ### HelloWorld
 # helloWorld = HelloWorldAgent
 # eventsList = helloWorld.__guard_io_sarl_core_Initialize__(helloWorld,1)
 # eventsList[0](helloWorld,1)
 
 ### HelloWorld with the loggingSkill
-loggingAgent = LoggingAgent(1, 2)
+loggingAgent = LoggingAgent(None, uuid.uuid4())
 myAgentEvents = loggingAgent.__guard_io_sarl_core_Initialize__(1)
 myAgentEvents[0](1)
 
 ### GuardedAgent
-guardedAgent = GuardedAgent(1, 2)
+guardedAgent = GuardedAgent(None, uuid.uuid4())
 myAgentEvents1 = guardedAgent.__guard_io_sarl_core_Initialize__(2)
 myAgentEvents1[0](2)
