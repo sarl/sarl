96e4607b08485c5a6fa75433871d1ce8494063f2
Stéphane Galland <galland@arakhne.org>
2024-12-01 14:05:52 +0800

    Add missed tests for the generic event serialization.
    Signed-off-by: Stéphane Galland <galland@arakhne.org>

--- sarl-lang/compiler.tests/src/test/java/io/sarl/lang/tests/modules/serializer/EventSerializerTest.java
+++ sarl-lang/compiler.tests/src/test/java/io/sarl/lang/tests/modules/serializer/EventSerializerTest.java
@@ -23,6 +23,7 @@ package io.sarl.lang.tests.modules.serializer;
 import static io.sarl.lang.tests.api.tools.TestEObjects.event;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -36,34 +37,338 @@ import org.junit.jupiter.api.Test;
 @Tag("core")
 @Tag("serialization")
 @SuppressWarnings("all")
-public class EventSerializerTest extends AbstractSerializerTest {
+public class EventSerializerTest {
 
-	@Test
-	public void empty_noBlock_noSuper() throws Exception {
-		String s = "event Foo"; //$NON-NLS-1$
-		this.object = event(getParseHelper(), getValidationHelper(), s);
-		assertSerialize(s);
+	@DisplayName("Without type parameter")
+	@Nested
+	public class WithoutTypeParameter extends AbstractSerializerTest {
+		
+		@Test
+		@DisplayName("event")
+		public void empty_noBlock_noSuper() throws Exception {
+			String s = "event Foo"; //$NON-NLS-1$
+			this.object = event(getParseHelper(), getValidationHelper(), s);
+			assertSerialize(s);
+		}
+
+		@Test
+		@DisplayName("event {}")
+		public void empty_block_noSuper() throws Exception {
+			String s = "event Foo { }"; //$NON-NLS-1$
+			this.object = event(getParseHelper(), getValidationHelper(), s);
+			assertSerialize(s);
+		}
+
+		@Test
+		@DisplayName("event extends")
+		public void empty_noBlock_super() throws Exception {
+			String s = "event Foo extends foo.ecore.SubEvent"; //$NON-NLS-1$
+			this.object = event(getParseHelper(), getValidationHelper(), s);
+			assertSerialize(s);
+		}
+
+		@Test
+		@DisplayName("event extends {}")
+		public void empty_block_super() throws Exception {
+			String s = "event Foo extends foo.ecore.SubEvent { }"; //$NON-NLS-1$
+			this.object = event(getParseHelper(), getValidationHelper(), s);
+			assertSerialize(s);
+		}
+
 	}
 
-	@Test
-	public void empty_block_noSuper() throws Exception {
-		String s = "event Foo { }"; //$NON-NLS-1$
-		this.object = event(getParseHelper(), getValidationHelper(), s);
-		assertSerialize(s);
-	}
+	@DisplayName("With type parameters")
+	@Nested
+	public class WithTypeParameters extends AbstractSerializerTest {
 
-	@Test
-	public void empty_noBlock_super() throws Exception {
-		String s = "event Foo extends foo.ecore.SubEvent"; //$NON-NLS-1$
-		this.object = event(getParseHelper(), getValidationHelper(), s);
-		assertSerialize(s);
-	}
+		@Test
+		@DisplayName("event<A>")
+		public void empty_noBlock_noSuper_0() throws Exception {
+			String s = "event Foo<A>"; //$NON-NLS-1$
+			this.object = event(getParseHelper(), getValidationHelper(), s);
+			assertSerialize(s);
+		}
 
-	@Test
-	public void empty_block_super() throws Exception {
-		String s = "event Foo extends foo.ecore.SubEvent { }"; //$NON-NLS-1$
-		this.object = event(getParseHelper(), getValidationHelper(), s);
-		assertSerialize(s);
+		@Test
+		@DisplayName("event<A extends Number>")
+		public void empty_noBlock_noSuper_1() throws Exception {
+			String s = "event Foo<A extends Number>"; //$NON-NLS-1$
+			this.object = event(getParseHelper(), getValidationHelper(), s);
+			assertSerialize(s);
+		}
+
+		@Test
+		@DisplayName("event<A extends Number, B>")
+		public void empty_noBlock_noSuper_2() throws Exception {
+			String s = "event Foo<A extends Number, B>"; //$NON-NLS-1$
+			this.object = event(getParseHelper(), getValidationHelper(), s);
+			assertSerialize(s);
+		}
+
+		@Test
+		@DisplayName("event<A extends Number, B extends String>")
+		public void empty_noBlock_noSuper_3() throws Exception {
+			String s = "event Foo<A extends Number, B extends String>"; //$NON-NLS-1$
+			this.object = event(getParseHelper(), getValidationHelper(), s);
+			assertSerialize(s);
+		}
+
+		@Test
+		@DisplayName("event<A, B extends String>")
+		public void empty_noBlock_noSuper_4() throws Exception {
+			String s = "event Foo<A, B extends String>"; //$NON-NLS-1$
+			this.object = event(getParseHelper(), getValidationHelper(), s);
+			assertSerialize(s);
+		}
+
+		@Test
+		@DisplayName("event<A, B>")
+		public void empty_noBlock_noSuper_5() throws Exception {
+			String s = "event Foo<A, B>"; //$NON-NLS-1$
+			this.object = event(getParseHelper(), getValidationHelper(), s);
+			assertSerialize(s);
+		}
+
+		@Test
+		@DisplayName("event<A> {}")
+		public void empty_block_noSuper_0() throws Exception {
+			String s = "event Foo<A> { }"; //$NON-NLS-1$
+			this.object = event(getParseHelper(), getValidationHelper(), s);
+			assertSerialize(s);
+		}
+
+		@Test
+		@DisplayName("event<A extends Number> {}")
+		public void empty_block_noSuper_1() throws Exception {
+			String s = "event Foo<A extends Number> { }"; //$NON-NLS-1$
+			this.object = event(getParseHelper(), getValidationHelper(), s);
+			assertSerialize(s);
+		}
+
+		@Test
+		@DisplayName("event<A extends Number, B> {}")
+		public void empty_block_noSuper_2() throws Exception {
+			String s = "event Foo<A extends Number, B> { }"; //$NON-NLS-1$
+			this.object = event(getParseHelper(), getValidationHelper(), s);
+			assertSerialize(s);
+		}
+
+		@Test
+		@DisplayName("event<A extends Number, B extends String> {}")
+		public void empty_block_noSuper_3() throws Exception {
+			String s = "event Foo<A extends Number, B extends String> { }"; //$NON-NLS-1$
+			this.object = event(getParseHelper(), getValidationHelper(), s);
+			assertSerialize(s);
+		}
+
+		@Test
+		@DisplayName("event<A, B extends String> {}")
+		public void empty_block_noSuper_4() throws Exception {
+			String s = "event Foo<A, B extends String> { }"; //$NON-NLS-1$
+			this.object = event(getParseHelper(), getValidationHelper(), s);
+			assertSerialize(s);
+		}
+
+		@Test
+		@DisplayName("event<A, B> {}")
+		public void empty_block_noSuper_5() throws Exception {
+			String s = "event Foo<A, B> { }"; //$NON-NLS-1$
+			this.object = event(getParseHelper(), getValidationHelper(), s);
+			assertSerialize(s);
+		}
+
+		@Test
+		@DisplayName("event<A> extends")
+		public void empty_noBlock_super_0() throws Exception {
+			String s = "event Foo<A> extends foo.ecore.SubEvent"; //$NON-NLS-1$
+			this.object = event(getParseHelper(), getValidationHelper(), s);
+			assertSerialize(s);
+		}
+
+		@Test
+		@DisplayName("event<A extends Number> extends")
+		public void empty_noBlock_super_1() throws Exception {
+			String s = "event Foo<A extends Number> extends foo.ecore.SubEvent"; //$NON-NLS-1$
+			this.object = event(getParseHelper(), getValidationHelper(), s);
+			assertSerialize(s);
+		}
+
+		@Test
+		@DisplayName("event<A extends Number, B> extends")
+		public void empty_noBlock_super_2() throws Exception {
+			String s = "event Foo<A extends Number, B> extends foo.ecore.SubEvent"; //$NON-NLS-1$
+			this.object = event(getParseHelper(), getValidationHelper(), s);
+			assertSerialize(s);
+		}
+
+		@Test
+		@DisplayName("event<A extends Number, B extends String> extends")
+		public void empty_noBlock_super_3() throws Exception {
+			String s = "event Foo<A extends Number, B extends String> extends foo.ecore.SubEvent"; //$NON-NLS-1$
+			this.object = event(getParseHelper(), getValidationHelper(), s);
+			assertSerialize(s);
+		}
+
+		@Test
+		@DisplayName("event<A, B extends String> extends")
+		public void empty_noBlock_super_4() throws Exception {
+			String s = "event Foo<A, B extends String> extends foo.ecore.SubEvent"; //$NON-NLS-1$
+			this.object = event(getParseHelper(), getValidationHelper(), s);
+			assertSerialize(s);
+		}
+
+		@Test
+		@DisplayName("event<A, B> extends")
+		public void empty_noBlock_super_5() throws Exception {
+			String s = "event Foo<A, B> extends foo.ecore.SubEvent"; //$NON-NLS-1$
+			this.object = event(getParseHelper(), getValidationHelper(), s);
+			assertSerialize(s);
+		}
+
+		@Test
+		@DisplayName("event<A> extends {}")
+		public void empty_block_super_0() throws Exception {
+			String s = "event Foo<A> extends foo.ecore.SubEvent { }"; //$NON-NLS-1$
+			this.object = event(getParseHelper(), getValidationHelper(), s);
+			assertSerialize(s);
+		}
+
+		@Test
+		@DisplayName("event<A extends Number> extends {}")
+		public void empty_block_super_1() throws Exception {
+			String s = "event Foo<A extends Number> extends foo.ecore.SubEvent { }"; //$NON-NLS-1$
+			this.object = event(getParseHelper(), getValidationHelper(), s);
+			assertSerialize(s);
+		}
+
+		@Test
+		@DisplayName("event<A extends Number, B> extends {}")
+		public void empty_block_super_2() throws Exception {
+			String s = "event Foo<A extends Number, B> extends foo.ecore.SubEvent { }"; //$NON-NLS-1$
+			this.object = event(getParseHelper(), getValidationHelper(), s);
+			assertSerialize(s);
+		}
+
+		@Test
+		@DisplayName("event<A extends Number, B extends String> extends {}")
+		public void empty_block_super_3() throws Exception {
+			String s = "event Foo<A extends Number, B extends String> extends foo.ecore.SubEvent { }"; //$NON-NLS-1$
+			this.object = event(getParseHelper(), getValidationHelper(), s);
+			assertSerialize(s);
+		}
+
+		@Test
+		@DisplayName("event<A, B extends String> extends {}")
+		public void empty_block_super_4() throws Exception {
+			String s = "event Foo<A, B extends String> extends foo.ecore.SubEvent { }"; //$NON-NLS-1$
+			this.object = event(getParseHelper(), getValidationHelper(), s);
+			assertSerialize(s);
+		}
+
+		@Test
+		@DisplayName("event<A, B> extends {}")
+		public void empty_block_super_5() throws Exception {
+			String s = "event Foo<A,B> extends foo.ecore.SubEvent { }"; //$NON-NLS-1$
+			this.object = event(getParseHelper(), getValidationHelper(), s);
+			assertSerialize(s);
+		}
+
+		@Test
+		@DisplayName("event<A> extends <Double>")
+		public void empty_noBlock_supergen_0() throws Exception {
+			String s = "event Foo<A> extends foo.ecore.SubGenEvent<Double>"; //$NON-NLS-1$
+			this.object = event(getParseHelper(), getValidationHelper(), s);
+			assertSerialize(s);
+		}
+
+		@Test
+		@DisplayName("event<A extends Number> extends <A>")
+		public void empty_noBlock_supergen_1() throws Exception {
+			String s = "event Foo<A extends Number> extends foo.ecore.SubGenEvent<A>"; //$NON-NLS-1$
+			this.object = event(getParseHelper(), getValidationHelper(), s);
+			assertSerialize(s);
+		}
+
+		@Test
+		@DisplayName("event<A extends Number, B> extends <A>")
+		public void empty_noBlock_supergen_2() throws Exception {
+			String s = "event Foo<A extends Number, B> extends foo.ecore.SubGenEvent<A>"; //$NON-NLS-1$
+			this.object = event(getParseHelper(), getValidationHelper(), s);
+			assertSerialize(s);
+		}
+
+		@Test
+		@DisplayName("event<A extends Number, B extends String> extends <A>")
+		public void empty_noBlock_supergen_3() throws Exception {
+			String s = "event Foo<A extends Number, B extends String> extends foo.ecore.SubGenEvent<A>"; //$NON-NLS-1$
+			this.object = event(getParseHelper(), getValidationHelper(), s);
+			assertSerialize(s);
+		}
+
+		@Test
+		@DisplayName("event<A, B extends String> extends <Double>")
+		public void empty_noBlock_supergen_4() throws Exception {
+			String s = "event Foo<A, B extends String> extends foo.ecore.SubGenEvent<Double>"; //$NON-NLS-1$
+			this.object = event(getParseHelper(), getValidationHelper(), s);
+			assertSerialize(s);
+		}
+
+		@Test
+		@DisplayName("event<A, B> extends <Double>")
+		public void empty_noBlock_supergen_5() throws Exception {
+			String s = "event Foo<A, B> extends foo.ecore.SubGenEvent<Double>"; //$NON-NLS-1$
+			this.object = event(getParseHelper(), getValidationHelper(), s);
+			assertSerialize(s);
+		}
+
+		@Test
+		@DisplayName("event<A> extends <Double> {}")
+		public void empty_block_supergen_0() throws Exception {
+			String s = "event Foo<A> extends foo.ecore.SubGenEvent<Double> { }"; //$NON-NLS-1$
+			this.object = event(getParseHelper(), getValidationHelper(), s);
+			assertSerialize(s);
+		}
+
+		@Test
+		@DisplayName("event<A extends Number> extends <A> {}")
+		public void empty_block_superen_1() throws Exception {
+			String s = "event Foo<A extends Number> extends foo.ecore.SubGenEvent<A> { }"; //$NON-NLS-1$
+			this.object = event(getParseHelper(), getValidationHelper(), s);
+			assertSerialize(s);
+		}
+
+		@Test
+		@DisplayName("event<A extends Number, B> extends <A> {}")
+		public void empty_block_supergen_2() throws Exception {
+			String s = "event Foo<A extends Number, B> extends foo.ecore.SubGenEvent<A> { }"; //$NON-NLS-1$
+			this.object = event(getParseHelper(), getValidationHelper(), s);
+			assertSerialize(s);
+		}
+
+		@Test
+		@DisplayName("event<A extends Number, B extends String> extends <A> {}")
+		public void empty_block_supergen_3() throws Exception {
+			String s = "event Foo<A extends Number, B extends String> extends foo.ecore.SubGenEvent<A> { }"; //$NON-NLS-1$
+			this.object = event(getParseHelper(), getValidationHelper(), s);
+			assertSerialize(s);
+		}
+
+		@Test
+		@DisplayName("event<A, B extends String> extends <Double> {}")
+		public void empty_block_supergen_4() throws Exception {
+			String s = "event Foo<A, B extends String> extends foo.ecore.SubGenEvent<Double> { }"; //$NON-NLS-1$
+			this.object = event(getParseHelper(), getValidationHelper(), s);
+			assertSerialize(s);
+		}
+
+		@Test
+		@DisplayName("event<A, B> extends <Double> {}")
+		public void empty_block_supergen_5() throws Exception {
+			String s = "event Foo<A,B> extends foo.ecore.SubGenEvent<Double> { }"; //$NON-NLS-1$
+			this.object = event(getParseHelper(), getValidationHelper(), s);
+			assertSerialize(s);
+		}
+
 	}
 
 }
